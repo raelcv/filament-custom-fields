36531d74e84d2a1a8bd036b4b314746091db6328
Artjoms Usakovs <ushakov09@gmail.com>
2025-01-20 21:28:54 +0200

    Added Latvian and Ukranian translations

---
+++ resources/lang/lv/custom-fields.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'designation' => 'Nozīmējums',
+    'label' => 'Nosaukums',
+    'main_info' => 'Pamatinformācija',
+    'group' => 'Grupa',
+    'placeholder' => 'Viettura teksts',
+    'field_type' => 'Lauciņa tips',
+    'description' => 'Apraksts',
+    'settings' => 'Iestatījumi',
+    'required' => 'Obligāts',
+    'min' => 'Min',
+    'max' => 'Max',
+    'order' => 'Kārtas numurs',
+    'is_table_visible' => 'Redzams tabulā',
+    'is_sortable' => 'Kārtoams',
+    'is_searchable' => 'Meklējams',
+
+    'placeholder_placeholder' => 'Viettura teksts',
+    'order_placeholder' => 'Kārtas numurs',
+    'description_placeholder' => 'Apraksts',
+    'min_placeholder' => 'Min',
+    'max_placeholder' => 'Max',
+
+    'options' => 'Opcijas',
+];
---
+++ resources/lang/ua/custom-fields.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'designation' => 'Позначення',
+    'label' => 'Мітка',
+    'main_info' => 'Основна інформація',
+    'group' => 'Група',
+    'placeholder' => 'Заповнювач',
+    'field_type' => 'Тип поля',
+    'description' => 'Опис',
+    'settings' => 'Налаштування',
+    'required' => 'Обов’язкове',
+    'min' => 'Мінімум',
+    'max' => 'Максимум',
+    'order' => 'Порядок',
+    'is_table_visible' => 'Видимий у таблиці',
+    'is_sortable' => 'Сортується',
+    'is_searchable' => 'Пошуковий',
+
+    'placeholder_placeholder' => 'Заповнювач',
+    'order_placeholder' => 'Порядок',
+    'description_placeholder' => 'Опис',
+    'min_placeholder' => 'Мінімум',
+    'max_placeholder' => 'Максимум',
+
+    'options' => 'Опції',
+];
