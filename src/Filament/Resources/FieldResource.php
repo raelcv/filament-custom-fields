68b439a4058212cd3c18d189e7562b6dec66ea3f
Artjoms Usakovs <ushakov09@gmail.com>
2025-01-20 21:18:49 +0200

    Fixed missing translation file

--- src/Filament/Resources/FieldResource.php
+++ src/Filament/Resources/FieldResource.php
@@ -26,40 +26,40 @@ class FieldResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Section::make(__('custom-fields.designation'))
+                Forms\Components\Section::make(trans('custom-fields::custom-fields.designation'))
                     ->schema([
                         Forms\Components\Select::make('designation')
-                            ->label(__('custom-fields.designation'))
+                            ->label(trans('custom-fields::custom-fields.custom-fields.designation'))
                             ->options(config('custom-fields.field_designations'))
                             ->required(),
                     ]),
 
-                Forms\Components\Section::make(__('custom-fields.main_info'))
+                Forms\Components\Section::make(trans('custom-fields::custom-fields.main_info'))
                     ->columns()
                     ->schema([
                         Forms\Components\TextInput::make('label')
-                            ->label(__('custom-fields.label'))
+                            ->label(trans('custom-fields::custom-fields.label'))
                             ->required()
                             ->placeholder('Enter the name of the field'),
 
                         Forms\Components\TextInput::make('placeholder')
-                            ->label(__('custom-fields.placeholder'))
+                            ->label(trans('custom-fields::custom-fields.placeholder'))
                             ->maxLength(255)
-                            ->placeholder(__('custom-fields.placeholder_placeholder')),
+                            ->placeholder(trans('custom-fields::custom-fields.placeholder_placeholder')),
 
                         Forms\Components\TextInput::make('group')
-                            ->label(__('custom-fields.group'))
+                            ->label(trans('custom-fields::custom-fields.group'))
                             ->required()
-                            ->placeholder(__('custom-fields.group_placeholder')),
+                            ->placeholder(trans('custom-fields::custom-fields.group_placeholder')),
 
                         NumberInput::make('order')
-                            ->label(__('custom-fields.order'))
+                            ->label(trans('custom-fields::custom-fields.order'))
                             ->minValue(0)
                             ->default(0)
-                            ->placeholder(__('custom-fields.order_placeholder')),
+                            ->placeholder(trans('custom-fields::custom-fields.order_placeholder')),
 
                         Forms\Components\Select::make('field_type')
-                            ->label(__('custom-fields.field_type'))
+                            ->label(trans('custom-fields::custom-fields.field_type'))
                             ->options(FieldType::toSelect())
                             ->reactive()
                             ->afterStateUpdated(
@@ -71,23 +71,23 @@ class FieldResource extends Resource
                             ->required(),
 
                         Forms\Components\Textarea::make('description')
-                            ->label(__('custom-fields.description'))
-                            ->placeholder(__('custom-fields.description_placeholder')),
+                            ->label(trans('custom-fields::custom-fields.description'))
+                            ->placeholder(trans('custom-fields::custom-fields.description_placeholder')),
                     ]),
 
-                Forms\Components\Section::make(__('custom-fields.options'))
+                Forms\Components\Section::make(trans('custom-fields::custom-fields.options'))
                     ->hidden(static fn (callable $get) => ! $get('has_options'))
                     ->schema([
                         Forms\Components\Repeater::make('options')
-                            ->label(__('custom-fields.options'))
-                            ->addActionLabel(__('custom-fields.add_option'))
+                            ->label(trans('custom-fields::custom-fields.options'))
+                            ->addActionLabel(trans('custom-fields::custom-fields.add_option'))
                             ->schema([
                                 Forms\Components\TextInput::make('label')
-                                    ->label(__('custom-fields.label'))
+                                    ->label(trans('custom-fields::custom-fields.label'))
                                     ->required(),
 
                                 Forms\Components\TextInput::make('value')
-                                    ->label(__('custom-fields.value'))
+                                    ->label(trans('custom-fields::custom-fields.value'))
                                     ->required(),
                             ])
                             ->relationship('options')
@@ -95,30 +95,30 @@ class FieldResource extends Resource
                             ->collapsible(),
                     ]),
 
-                Forms\Components\Section::make(__('custom-fields.settings'))
+                Forms\Components\Section::make(trans('custom-fields::custom-fields.settings'))
                     ->columns(4)
                     ->schema([
                         Forms\Components\Toggle::make('required')
-                            ->label(__('custom-fields.required')),
+                            ->label(trans('custom-fields::custom-fields.required')),
 
                         Forms\Components\Toggle::make('is_table_visible')
-                            ->label(__('custom-fields.is_table_visible')),
+                            ->label(trans('custom-fields::custom-fields.is_table_visible')),
 
                         Forms\Components\Toggle::make('is_searchable')
-                            ->label(__('custom-fields.is_searchable')),
+                            ->label(trans('custom-fields::custom-fields.is_searchable')),
 
                         Forms\Components\Toggle::make('is_sortable')
-                            ->label(__('custom-fields.is_sortable')),
+                            ->label(trans('custom-fields::custom-fields.is_sortable')),
 
                         Forms\Components\TextInput::make('min')
-                            ->label(__('custom-fields.min'))
+                            ->label(trans('custom-fields::custom-fields.min'))
                             ->hidden(static fn (callable $get) => $get('has_options'))
-                            ->placeholder(__('custom-fields.min_placeholder')),
+                            ->placeholder(trans('custom-fields::custom-fields.min_placeholder')),
 
                         Forms\Components\TextInput::make('max')
-                            ->label(__('custom-fields.max'))
+                            ->label(trans('custom-fields::custom-fields.max'))
                             ->hidden(static fn (callable $get) => $get('has_options'))
-                            ->placeholder(__('custom-fields.max_placeholder')),
+                            ->placeholder(trans('custom-fields::custom-fields.max_placeholder')),
                     ]),
             ]);
     }
@@ -128,12 +128,12 @@ class FieldResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('label')
-                    ->label(__('custom-fields.label'))
+                    ->label(trans('custom-fields::custom-fields.label'))
                     ->searchable()
                     ->sortable(),
 
                 Tables\Columns\TextColumn::make('designation')
-                    ->label(__('custom-fields.designation'))
+                    ->label(trans('custom-fields::custom-fields.designation'))
                     ->getStateUsing(
                         static fn (Field $record): string => config('custom-fields.field_designations')[$record->designation]
                             ?? ''
@@ -141,42 +141,42 @@ class FieldResource extends Resource
                     ->sortable(),
 
                 Tables\Columns\TextColumn::make('group')
-                    ->label(__('custom-fields.group'))
+                    ->label(trans('custom-fields::custom-fields.group'))
                     ->searchable()
                     ->sortable(),
 
                 Tables\Columns\TextColumn::make('order')
-                    ->label(__('custom-fields.order'))
+                    ->label(trans('custom-fields::custom-fields.order'))
                     ->searchable()
                     ->sortable(),
 
                 Tables\Columns\TextColumn::make('field_type')
-                    ->label(__('custom-fields.field_type'))
+                    ->label(trans('custom-fields::custom-fields.field_type'))
                     ->searchable()
                     ->badge()
                     ->sortable(),
 
                 Tables\Columns\IconColumn::make('required')
-                    ->label(__('custom-fields.required'))
+                    ->label(trans('custom-fields::custom-fields.required'))
                     ->icon(static fn (Field $record): string => getIcon($record->required))
                     ->color(static fn (Field $record): string => getIconColor($record->required))
                     ->sortable(),
 
                 Tables\Columns\IconColumn::make('is_table_visible')
-                    ->label(__('custom-fields.is_table_visible'))
+                    ->label(trans('custom-fields::custom-fields.is_table_visible'))
                     ->icon(static fn (Field $record): string => getIcon($record->is_table_visible))
                     ->color(static fn (Field $record): string => getIconColor($record->is_table_visible))
                     ->sortable(),
 
                 Tables\Columns\IconColumn::make('is_searchable')
-                    ->label(__('custom-fields.is_searchable'))
+                    ->label(trans('custom-fields::custom-fields.is_searchable'))
                     ->icon(static fn (Field $record): string => getIcon($record->is_searchable))
                     ->color(static fn (Field $record): string => getIconColor($record->is_searchable))
                     ->sortable(),
             ])
             ->filters([
                 Tables\Filters\SelectFilter::make('designation')
-                    ->label(__('custom-fields.designation'))
+                    ->label(trans('custom-fields::custom-fields.designation'))
                     ->options(config('custom-fields.field_designations'))
                     ->query(static function (Builder $query, array $data): Builder {
                         $value = config('custom-fields.field_designations')[$data['value']] ?? '';
@@ -185,7 +185,7 @@ class FieldResource extends Resource
                     }),
 
                 Tables\Filters\SelectFilter::make('group')
-                    ->label(__('custom-fields.group'))
+                    ->label(trans('custom-fields::custom-fields.group'))
                     ->options(Field::pluck('group', 'group')->filter()->unique())
                     ->query(static function (Builder $query, array $data): Builder {
                         return $query->where('group', 'like', "%{$data['value']}%");
