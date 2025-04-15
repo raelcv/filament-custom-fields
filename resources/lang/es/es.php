9b26d7d51c6e29bc2fbf5266767652dceae37a0c
David Rey <neisserrey@upeu.edu.pe>
2025-04-15 10:47:02 -0500

    feat: :globe_with_meridians: add ES translation

---
+++ resources/lang/es/es.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'designation' => 'Designación',
+    'label' => 'Etiqueta',
+    'main_info' => 'Información Principal',
+    'group' => 'Grupo',
+    'placeholder' => 'Marcador de posición',
+    'field_type' => 'Tipo de Campo',
+    'description' => 'Descripción',
+    'settings' => 'Configuraciones',
+    'required' => 'Requerido',
+    'min' => 'Mín',
+    'max' => 'Máx',
+    'order' => 'Orden',
+    'is_table_visible' => 'Visible en Tabla',
+    'is_sortable' => 'Ordenable',
+    'is_searchable' => 'Buscable',
+
+    'placeholder_placeholder' => 'Marcador de posición',
+    'order_placeholder' => 'Orden',
+    'description_placeholder' => 'Descripción',
+    'min_placeholder' => 'Mín',
+    'max_placeholder' => 'Máx',
+
+    'options' => 'Opciones',
+];
