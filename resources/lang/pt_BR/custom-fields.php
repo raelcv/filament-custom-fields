eafb13c4f2acea76096a56c79e6557eb20321085
raelcv <raelcv@gmail.com>
2025-08-24 11:36:05 -0300

    feat: :globe_with_meridians: add portuguese translation

---
+++ resources/lang/pt_BR/custom-fields.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'designation' => 'Designação',
+    'label' => 'Rótulo',
+    'main_info' => 'Informações Principais',
+    'group' => 'Grupo',
+    'placeholder' => 'Placeholder',
+    'field_type' => 'Tipo de Campo',
+    'description' => 'Descrição',
+    'settings' => 'Configurações',
+    'required' => 'Obrigatório',
+    'min' => 'Mínimo',
+    'max' => 'Máximo',
+    'order' => 'Ordem',
+    'is_table_visible' => 'Visível na Tabela',
+    'is_sortable' => 'Ordenável',
+    'is_searchable' => 'Pesquisável',
+
+    'placeholder_placeholder' => 'Placeholder',
+    'order_placeholder' => 'Ordem',
+    'description_placeholder' => 'Descrição',
+    'min_placeholder' => 'Mínimo',
+    'max_placeholder' => 'Máximo',
+
+    'options' => 'Opções',
+];
